5fc275a636751c9cd0b816111a507bb7182cb665
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-10 11:22:27 -0600

    The friends endpoint was created.

--- server.js
+++ server.js
@@ -2,8 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hi!");
+const friends = [
+  {
+    id: 0,
+    name: "Alber Einstein",
+  },
+  {
+    id: 1,
+    name: "Sr Isaac Newton",
+  },
+];
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:id", (req, res) => {
+  const friendId = Number(req.params.id);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist.",
+    });
+  }
 });
 
 app.get("/messages", (req, res) => {
