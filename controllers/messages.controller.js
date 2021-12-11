a31b6c66951725768a7a90b847586a2db7390c77
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-11 11:25:20 -0600

    The messages controller returns a file.

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 function getMessages(req, res) {
-  res.send("<ul><li>Hello Albert!</li></ul>");
+  res.sendFile(path.join(__dirname, "..", "public", "panchitoPistolas.jpg"));
+  // res.send("<ul><li>Hello Albert!</li></ul>");
 }
 
 function postMessages(req, res) {
