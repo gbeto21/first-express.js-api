464b98d04e682d17e046da98965040c6e5ec11ed
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-11 12:34:56 -0600

    A template was added.

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,9 +1,13 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  res.sendFile(
-    path.join(__dirname, "..", "public", "images", "panchitoPistolas.jpg")
-  );
+  // res.sendFile(
+  //   path.join(__dirname, "..", "public", "images", "panchitoPistolas.jpg")
+  // );
+  res.render("messages", {
+    title: "Messages to my friends!",
+    friend: "Elon Musk",
+  });
 }
 
 function postMessages(req, res) {
